0e2cc13812d25c54663b4f82b6bf7720cc73777d
T. Bunratta <tharathorn.bu@ku.th>
2019-09-08 16:10:10 +0700

    Add basic routing and request handling for Part 3

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ by Tharathorn Bunrattanasathian
 |:---------:|:----------:|:---:|
 |I| Project, app, and first view creation |✓|
 |II| Database and model setup, migrations, Django model API, and site administration basics |✓|
-|III| More about responsive views, templates, request handling, and URL namespacing |   |
+|III| More about responsive views, templates, request handling, and URL routing |✓|
 |IV| Simple forms, generic and armed views |   |
 |V| All about testing basics in Django  |   |
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
 
@@ -10,7 +11,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
