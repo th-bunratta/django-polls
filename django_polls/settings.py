2536f98a42c590216a51caf26d0ff623f7e99dcf
T. Bunratta <tharathorn.bu@ku.th>
2019-09-08 15:06:37 +0700

    First view creation

--- django_polls/settings.py
+++ django_polls/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8kan1sqp*brq6pm+m0luvl_pg3^!1z5y2(nmz671m)^48ho64-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
