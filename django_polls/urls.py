c79db79b24f6ba865fe0096f650798565931fff1
T. Bunratta <tharathorn.bu@ku.th>
2019-09-23 15:48:11 +0700

    Fix Polls app routes

--- django_polls/urls.py
+++ django_polls/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from polls import views
 
 urlpatterns = [
-    path('', include('polls.urls')),
+    path('', views.IndexView.as_view()),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
 ]
