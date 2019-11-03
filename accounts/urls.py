aaec69202c45fcf3051a0c540ad4dfc5440a9d3b
T. Bunratta <tharathorn.bu@ku.th>
2019-11-04 00:26:26 +0700

    Allow authenticated user redirect on signing in

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    # path('', include('django.contrib.auth.urls')),
+    path('login/', views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
 
