09974386bd27515952f14ce6b7f6ce4836b53b1f
T. Bunratta <tharathorn.bu@ku.th>
2019-11-03 23:43:10 +0700

    Register Vote model for administration

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Choice, Question
+from .models import Choice, Question, Vote
 
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Vote)
