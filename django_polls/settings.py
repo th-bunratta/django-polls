e8f2f6d9d3588bb96e769411afeee783b0286313
T. Bunratta <tharathorn.bu@ku.th>
2019-11-04 11:19:39 +0700

    Add Django Model authentication backend

--- django_polls/settings.py
+++ django_polls/settings.py
@@ -15,9 +15,12 @@ from dj_database_url import parse as db_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).parent # os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).parent  # os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
