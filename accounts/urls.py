b6a94ce47c15bab607b4341240c648366ba9b93d
T. Bunratta <tharathorn.bu@ku.th>
2019-11-04 10:27:12 +0700

    Register the registration route to the Accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from . import views
+from .views import register
 from django.contrib.auth import views
 
 app_name = 'accounts'
@@ -7,5 +7,6 @@ urlpatterns = [
     # path('', include('django.contrib.auth.urls')),
     path('login/', views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register')
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.signals import user_logged_out
 from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 def successful_logout_message(sender, user, request, **kwargs):
     messages.info(request, 'You have successfully been signed out.')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('polls:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 
 user_logged_out.connect(successful_logout_message)
